f0a4d39797d8fd1c08311659bff097e3607af060
ilhamsj <ilhamsaputrajati@gmail.com>
2019-12-06 17:57:52 +0700

    secure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_url('css/app.css') }}" rel="stylesheet">
     @stack('styles')
 </head>
 <body>
